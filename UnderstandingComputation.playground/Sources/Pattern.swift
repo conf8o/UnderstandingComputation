d9576d64f89c54b01ee8791aaee61a131615fbff
conf8o <8confeito8@gmail.com>
2021-10-04 23:59:32 +0900

    implement ToNFA for some pattern

--- UnderstandingComputation.playground/Sources/Pattern.swift
+++ UnderstandingComputation.playground/Sources/Pattern.swift
@@ -19,26 +19,32 @@ extension Pattern where Self: ToNFA {
     }
 }
 
-struct Empty: Pattern, ToNFA {
+struct Empty: Pattern {
     var description: String { "" }
     var precedence: Int { 3 }
+}
+
+extension Empty: ToNFA {
     func toNFADesign() -> NFADesign {
         let startState = State.new
         let acceptStates: Set<State> = [startState]
-        let mapping = NFATransitionMap([])
+        let mapping = NFATransitionMap(Dictionary())
         return NFADesign(startState: startState, acceptStates: acceptStates, transMap: mapping)
     }
 }
 
-struct Literal: Pattern, ToNFA {
+struct Literal: Pattern {
     var char: Character
     var description: String { String(char) }
     var precedence: Int { 3 }
+}
+
+extension Literal: ToNFA {
     func toNFADesign() -> NFADesign {
         let startState = State.new
         let acceptState = State.new
         let transition = (startState~Symbol.symbol(char))~>acceptState
-        let mapping = NFATransitionMap([transition])
+        let mapping = NFATransitionMap(transitions: [transition])
         return NFADesign(startState: startState, acceptStates: [acceptState], transMap: mapping)
     }
 }
@@ -52,6 +58,26 @@ struct Concatenate: Pattern {
     var precedence: Int { 1 }
 }
 
+extension Concatenate: ToNFA {
+    func toNFADesign() -> NFADesign {
+        let firstNFADesign = (first as! Pattern & ToNFA).toNFADesign()
+        let secondNFADesign = (second as! Pattern & ToNFA).toNFADesign()
+        let startState = firstNFADesign.startState
+        let acceptStates = secondNFADesign.acceptStates
+        let mapping = firstNFADesign.transMap.merging(secondNFADesign.transMap)
+        let extraMapping = NFATransitionMap(
+            transitions: firstNFADesign.acceptStates.map { state in
+                (state~Symbol.free)~>secondNFADesign.startState
+            }
+        )
+        return NFADesign(
+            startState: startState,
+            acceptStates: acceptStates,
+            transMap: mapping.merging(extraMapping)
+        )
+    }
+}
+
 struct Choose: Pattern {
     var first: Pattern
     var second: Pattern
@@ -59,6 +85,27 @@ struct Choose: Pattern {
         [first, second].map { pattern in pattern.bracket(self.precedence) }.joined(separator: "|")
     }
     var precedence: Int { 0 }
+}
+
+extension Choose: ToNFA {
+    func toNFADesign() -> NFADesign {
+        let firstNFADesign = (first as! Pattern & ToNFA).toNFADesign()
+        let secondNFADesing = (second as! Pattern & ToNFA).toNFADesign()
+        
+        let startState = State.new
+        let acceptStates = firstNFADesign.acceptStates.union(secondNFADesing.acceptStates)
+        let mapping = firstNFADesign.transMap.merging(secondNFADesing.transMap)
+        let extraMapping = NFATransitionMap(
+            transitions: [
+                (startState~Symbol.free)~>firstNFADesign.startState,
+                (startState~Symbol.free)~>secondNFADesing.startState
+            ]
+        )
+        return NFADesign(
+            startState: startState,
+            acceptStates: acceptStates,
+            transMap: mapping.merging(extraMapping))
+    }
 }
 
 struct Repeat: Pattern {
@@ -70,9 +117,15 @@ struct Repeat: Pattern {
 }
 
 public func patternMain() {
-    let pattern = Repeat(pattern: Choose(first: Concatenate(first: Literal(char: "a"),
-                                                            second: Literal(char: "b")),
-                                         second: Literal(char: "a")))
+    let pattern = Repeat(
+        pattern: Choose(
+            first: Concatenate(
+                first: Literal(char: "a"),
+                second: Literal(char: "b")
+            ),
+            second: Literal(char: "a")
+        )
+    )
     print(pattern)
     
     var d = Empty().toNFADesign()
@@ -86,4 +139,15 @@ public func patternMain() {
     
     print(Empty().matches("a"))
     print(Literal(char: "a").matches("a"))
+    
+    d = Concatenate(
+        first: Literal(char: "a"),
+        second: Concatenate(
+            first: Literal(char: "b"),
+            second: Literal(char: "c")
+        )
+    ).toNFADesign()
+    print(d.canAccept(str: "a"))
+    print(d.canAccept(str: "ab"))
+    print(d.canAccept(str: "abc"))
 }
